5a0fee469257228b215ad31d8f5205d796979f81
toyamagu2021 <toyamagu2021@gmail.com>
2024-01-28 18:44:18 +0900

    first commit
    Signed-off-by: toyamagu2021 <toyamagu2021@gmail.com>

---
+++ otel-with-dd/terraform/main.tf
@@ -0,0 +1,49 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
+
+  cluster_name    = local.name
+  cluster_version = local.cluster_version
+
+  cluster_endpoint_public_access = true
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
+
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.private_subnets
+
+  # EKS Managed Node Group(s)
+  eks_managed_node_group_defaults = {
+    instance_types = ["t3.large"]
+  }
+
+  eks_managed_node_groups = {
+    blue = {}
+    green = {
+      min_size     = 1
+      max_size     = 10
+      desired_size = 1
+
+      instance_types = ["t3.large"]
+      capacity_type  = "SPOT"
+    }
+  }
+
+  # aws-auth configmap
+  manage_aws_auth_configmap = true
+
+  aws_auth_accounts = [
+    data.aws_caller_identity.current.account_id
+  ]
+}
---
+++ otel-with-dd/terraform/variables.tf
@@ -0,0 +1,3 @@
+variable "owner" {
+  default = "tomoki-yamaguchi"
+}
